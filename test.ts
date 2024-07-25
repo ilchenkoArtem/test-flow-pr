1723c6362aacf7ebb09f3fb055da5deef86ac571
ilchenkoArtem <artem.ilchenko@711media.de>
2024-07-25 22:54:55 +0300

    fix

--- test.ts
+++ test.ts
@@ -2,51 +2,55 @@ import * as core from '@actions/core';
 import * as github from '@actions/github';
 import * as process from 'node:process';
 
-const TOKEN = process.env.GITHUB_TOKEN;
-const HEAD_BRANCH = process.env.HEAD_BRANCH; //from branch
-const BASE_BRANCH = process.env.BASE_BRANCH; //to branch
+const run = async () => {
 
-const octokit = github.getOctokit(TOKEN);
+  const TOKEN = process.env.GITHUB_TOKEN;
+  const HEAD_BRANCH = process.env.HEAD_BRANCH; //from branch
+  const BASE_BRANCH = process.env.BASE_BRANCH; //to branch
+
+  const octokit = github.getOctokit(TOKEN);
 
 
-core.info(`Fetching pull requests merged into develop from ${BASE_BRANCH}...`);
-const { data: closedPullRequestsByHeadBranch } = await octokit.rest.pulls.list({
-  owner: github.context.repo.owner,
-  repo: github.context.repo.repo,
-  state: 'closed',
-  head: HEAD_BRANCH,
-  base: "develop",
-  sort: 'updated',
-  direction: 'desc',
-});
+  core.info(`Fetching pull requests merged into develop from ${BASE_BRANCH}...`);
+  const {data: closedPullRequestsByHeadBranch} = await octokit.rest.pulls.list({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    state: 'closed',
+    head: HEAD_BRANCH,
+    base: "develop",
+    sort: 'updated',
+    direction: 'desc',
+  });
 
-if (closedPullRequestsByHeadBranch.length === 0) {
-  core.notice(`No pull requests found for ${HEAD_BRANCH}. Skipping...`);
-  process.exit()
-}
+  if (closedPullRequestsByHeadBranch.length === 0) {
+    core.notice(`No pull requests found for ${HEAD_BRANCH}. Skipping...`);
+    return;
+  }
 
-const mergedPullRequestsByHeadBranch = closedPullRequestsByHeadBranch.filter(
-  (pr) => !!pr.merged_at
-);
+  const mergedPullRequestsByHeadBranch = closedPullRequestsByHeadBranch.filter(
+    (pr) => !!pr.merged_at
+  );
 
-if (mergedPullRequestsByHeadBranch.length === 0) {
-  core.notice(`No merged pull requests found for ${HEAD_BRANCH}. Skipping...`);
-  process.exit()
-}
+  if (mergedPullRequestsByHeadBranch.length === 0) {
+    core.notice(`No merged pull requests found for ${HEAD_BRANCH}. Skipping...`);
+    process.exit()
+  }
 
-const lastMergedPullRequest = mergedPullRequestsByHeadBranch[0];
-const lastPullRequestMergeCommit = lastMergedPullRequest.merge_commit_sha;
+  const lastMergedPullRequest = mergedPullRequestsByHeadBranch[0];
+  const lastPullRequestMergeCommit = lastMergedPullRequest.merge_commit_sha;
 
-core.startGroup(`Last merged pull request info:`);
-core.info(`  URL: ${lastMergedPullRequest.html_url}`);
-core.info(`  Title: ${lastMergedPullRequest.title}`);
-core.info(`  Merged at: ${new Date(lastMergedPullRequest.merged_at).toLocaleDateString()}`);
-core.info(`  Merge commit SHA: ${lastPullRequestMergeCommit}`);
-core.endGroup()
+  core.startGroup(`Last merged pull request info:`);
+  core.info(`  URL: ${lastMergedPullRequest.html_url}`);
+  core.info(`  Title: ${lastMergedPullRequest.title}`);
+  core.info(`  Merged at: ${new Date(lastMergedPullRequest.merged_at).toLocaleDateString()}`);
+  core.info(`  Merge commit SHA: ${lastPullRequestMergeCommit}`);
+  core.endGroup()
 
-if(true) {
-  core.setFailed(`Merge commit SHA is not found for the last merged pull request`);
-}
+  if (true) {
+    core.setFailed(`Merge commit SHA is not found for the last merged pull request`);
+    return;
+  }
+};
 
-console.log('test1')
+run();
 
