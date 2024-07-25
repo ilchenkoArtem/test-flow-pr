95e2f9044bb7266ccffb1b59b3315110b090005f
ilchenkoArtem <artem.ilchenko@711media.de>
2024-07-25 21:31:55 +0300

    fix

--- test.ts
+++ test.ts
@@ -5,13 +5,10 @@ const TOKEN = process.env.GITHUB_TOKEN;
 const FROM_BRANCH = process.env.HEAD_BRANCH;
 const TO_BRANCH = process.env.BASE_BRANCH;
 
-console.log('FROM_BRANCH', FROM_BRANCH);
-console.log('TO_BRANCH', TO_BRANCH);
-
 const octokit = github.getOctokit(TOKEN);
 
 
-const { data: pulls } = await octokit.rest.pulls.list({
+const { data: closedPrByToBranch } = await octokit.rest.pulls.list({
   owner: github.context.repo.owner,
   repo: github.context.repo.repo,
   state: 'closed',
@@ -19,3 +16,5 @@ const { data: pulls } = await octokit.rest.pulls.list({
   sort: 'updated',
   direction: 'desc',
 });
+
+console.log('closedPrByToBranch', closedPrByToBranch);
