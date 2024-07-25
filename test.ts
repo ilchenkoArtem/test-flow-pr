cd3e581012905081a7139adce367be3e8f571265
ilchenkoArtem <artem.ilchenko@711media.de>
2024-07-25 21:53:22 +0300

    update

--- test.ts
+++ test.ts
@@ -8,7 +8,6 @@ const BASE_BRANCH = process.env.BASE_BRANCH; //to branch
 const octokit = github.getOctokit(TOKEN);
 
 
-console.log(`Fetching pull requests merged into develop from ${BASE_BRANCH}...`);
 const { data: closedPullRequestsByHeadBranch } = await octokit.rest.pulls.list({
   owner: github.context.repo.owner,
   repo: github.context.repo.repo,
