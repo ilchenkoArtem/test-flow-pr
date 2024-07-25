30afaaa1ab7cc008588e56e20090666342c0b81e
ilchenkoArtem <artem.ilchenko@711media.de>
2024-07-25 21:29:56 +0300

    fix

--- test.ts
+++ test.ts
@@ -2,8 +2,8 @@ import * as core from '@actions/core';
 import * as github from '@actions/github';
 
 const TOKEN = process.env.GITHUB_TOKEN;
-const FROM_BRANCH = process.env.FROM_BRANCH;
-const TO_BRANCH = process.env.TO_BRANCH;
+const FROM_BRANCH = process.env.BASE_BRANCH;
+const TO_BRANCH = process.env.HEAD_BRANCH;
 
 console.log('FROM_BRANCH', FROM_BRANCH);
 console.log('TO_BRANCH', TO_BRANCH);
