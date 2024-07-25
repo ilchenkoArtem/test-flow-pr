a16fa749c99f5524c02415e60dacc67449327530
ilchenkoArtem <artem.ilchenko@711media.de>
2024-07-25 21:49:30 +0300

    fix

--- test.ts
+++ test.ts
@@ -2,18 +2,34 @@ import * as core from '@actions/core';
 import * as github from '@actions/github';
 
 const TOKEN = process.env.GITHUB_TOKEN;
-const FROM_BRANCH = process.env.HEAD_BRANCH;
-const TO_BRANCH = process.env.BASE_BRANCH;
+const HEAD_BRANCH = process.env.HEAD_BRANCH; //from branch
+const BASE_BRANCH = process.env.BASE_BRANCH; //to branch
 
 const octokit = github.getOctokit(TOKEN);
 
 
-const { data: closedPrByToBranch } = await octokit.rest.pulls.list({
+core.info(`Fetching pull requests merged into develop from ${HEAD_BRANCH}...`);
+const { data: closedPullRequestsByHeadBranch } = await octokit.rest.pulls.list({
   owner: github.context.repo.owner,
   repo: github.context.repo.repo,
   state: 'closed',
+  head: HEAD_BRANCH,
+  base: "develop",
   sort: 'updated',
   direction: 'desc',
 });
 
-console.log('closedPrByToBranch', closedPrByToBranch);
+if (closedPullRequestsByHeadBranch.length === 0) {
+  core.info(`No pull requests found for ${HEAD_BRANCH}. Skipping...`);
+}
+
+const mergedPullRequestsByHeadBranch = closedPullRequestsByHeadBranch.filter(
+  (pr) => !!pr.merged_at
+);
+
+if (mergedPullRequestsByHeadBranch.length === 0) {
+  core.info(`No merged pull requests found for ${HEAD_BRANCH}. Skipping...`);
+}
+
+const lastMergedPullRequest = mergedPullRequestsByHeadBranch[0];
+
