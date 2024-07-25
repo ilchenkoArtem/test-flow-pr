702e63dc2835e372d268dc5b8808d2c5d7c80781
ilchenkoArtem <artem.ilchenko@711media.de>
2024-07-25 22:21:25 +0300

    fix

--- test.ts
+++ test.ts
@@ -33,8 +33,9 @@ if (mergedPullRequestsByHeadBranch.length === 0) {
 
 const lastMergedPullRequest = mergedPullRequestsByHeadBranch[0];
 
-core.info(`Last merged pull request:`);
+core.startGroup(`Last merged pull request:`);
 core.info(`  URL: ${lastMergedPullRequest.html_url}`);
 core.info(`  Title: ${lastMergedPullRequest.title}`);
 core.info(`  Merged at: ${new Date(lastMergedPullRequest.merged_at).toLocaleDateString()}`);
+core.endGroup()
 
