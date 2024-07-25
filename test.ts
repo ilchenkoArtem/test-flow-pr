ec9a9cf69ad582c4f5464345a291e4f50f335349
ilchenkoArtem <artem.ilchenko@711media.de>
2024-07-25 21:06:39 +0300

    fix

--- test.ts
+++ test.ts
@@ -2,5 +2,5 @@ import * as core from '@actions/core';
 
 
 
-const githubToken = core.getInput(process.env.GITHUB_TOKEN, { required: true });
-console.log('githubToken');
+const githubToken = process.env.GITHUB_TOKEN;
+console.log('githubToken', githubToken);
