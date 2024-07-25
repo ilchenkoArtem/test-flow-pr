ee2abcbc5abe6a8e38aab8b598eb5c0d3a6a5c9b
ilchenkoArtem <artem.ilchenko@711media.de>
2024-07-25 21:29:00 +0300

    fix

--- test.ts
+++ test.ts
@@ -1,8 +1,21 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
 
-const token = process.env.GITHUB_TOKEN;
-const octokit = github.getOctokit(token);
+const TOKEN = process.env.GITHUB_TOKEN;
+const FROM_BRANCH = process.env.FROM_BRANCH;
+const TO_BRANCH = process.env.TO_BRANCH;
+
+console.log('FROM_BRANCH', FROM_BRANCH);
+console.log('TO_BRANCH', TO_BRANCH);
+
+const octokit = github.getOctokit(TOKEN);
 
 
-
+const { data: pulls } = await octokit.rest.pulls.list({
+  owner: github.context.repo.owner,
+  repo: github.context.repo.repo,
+  state: 'closed',
+  base: TO_BRANCH,
+  sort: 'updated',
+  direction: 'desc',
+});
