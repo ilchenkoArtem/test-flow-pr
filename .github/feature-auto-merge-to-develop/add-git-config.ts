e563f714cf3e73d64aad5a86e8795bc4c478a931
ilchenkoArtem <artem.ilchenko@711media.de>
2024-07-29 20:55:36 +0300

    fix

--- .github/feature-auto-merge-to-develop/add-git-config.ts
+++ .github/feature-auto-merge-to-develop/add-git-config.ts
@@ -1,4 +1,5 @@
 import {$} from 'bun';
+import * as core from '@actions/core';
 import * as github from '@actions/github';
 
 interface AddGitConfigArgs {
@@ -6,9 +7,10 @@ interface AddGitConfigArgs {
 }
 
 export const addGitConfig = async ({gitHubToken}: AddGitConfigArgs) => {
+  core.info(`Adding git config...`);
   const repoFullName = `${github.context.repo.owner}/${github.context.repo.repo}`;
   await $`git remote set-url origin https://x-access-token:${gitHubToken}@github.com/${repoFullName}.git`
   await $`git config --global user.email "github-actions[bot]@users.noreply.github.com"`
   await $`git config --global user.name "github-actions[bot]"`
-
+  core.notice(`Git config has been added`)
 }
--- .github/feature-auto-merge-to-develop/squash-merge-commit.ts
+++ .github/feature-auto-merge-to-develop/squash-merge-commit.ts
@@ -1,5 +1,6 @@
 import {$} from 'bun';
 import {addGitConfig} from './add-git-config';
+import * as core from '@actions/core';
 
 interface MergeResult {
   merged: boolean;
@@ -24,13 +25,16 @@ const successMergeResult = (): MergeResult => {
 export const squashMergeCommit = async ({targetBranch, sourceBranch, commitMessage, gitHubToken}: SquashMergeCommitArgs) => {
   await addGitConfig({gitHubToken});
 
+  core.info(`Squash merging commit from ${sourceBranch} to ${targetBranch}...`);
   await $`git checkout ${targetBranch}`;
   const { stderr, exitCode} = await $`git merge --squash origin/${sourceBranch}`.nothrow()
 
   if (stderr.toString().includes("Automatic merge went well")) {
+    core.info(`Committing changes...`);
     await $`git commit -m "${commitMessage}"`;
     return successMergeResult();
   }
 
+  core.error(`Failed to merge commit from ${sourceBranch} to ${targetBranch}`);
   return failMergeResult(stderr.toString());
 }
