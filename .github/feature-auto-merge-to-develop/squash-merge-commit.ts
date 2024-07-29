7afbf470c6f6c19d2f863adbe513d89da6724e7a
Artem Ilchenko <33321841+ilchenkoArtem@users.noreply.github.com>
2024-07-29 21:58:54 +0300

    feat(FL-3002): Test Artme 3435 (#3)
    * add package.json
    
    * add base
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * fix
    
    * fix
    
    * fix
    
    * fix logs
    
    * fix logs
    
    * fix logs
    
    * fix logs
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * Revert "fix"
    
    This reverts commit c932396da343d8450cb52cb43bb1ca47f6c34376.
    
    * fix
    
    * fix
    
    * fix
    
    * add config
    
    * ашч
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix content
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix commit
    
    * fix commit
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix utils
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix test
    
    * fix
    
    * fix
    
    * fix errors
    
    * update scripts
    
    * add tests
    
    * remove unused data
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix

---
+++ .github/feature-auto-merge-to-develop/squash-merge-commit.ts
@@ -0,0 +1,50 @@
+import {$} from 'bun';
+import {addGitConfig} from './add-git-config';
+import * as core from '@actions/core';
+
+interface MergeResult {
+  merged: boolean;
+  error?: string;
+}
+
+interface SquashMergeCommitArgs {
+  targetBranch: string;
+  sourceBranch: string;
+  commitMessage: string;
+  gitHubToken: string;
+}
+
+const failMergeResult = (error: string): MergeResult => {
+  return {merged: false, error};
+}
+
+const successMergeResult = (): MergeResult => {
+  return {merged: true};
+}
+
+export const squashMergeCommit = async ({targetBranch, sourceBranch, commitMessage, gitHubToken}: SquashMergeCommitArgs) => {
+  await addGitConfig({gitHubToken});
+
+  core.info(`Squash merging commit from ${sourceBranch} to ${targetBranch}...`);
+  await $`git checkout ${targetBranch}`;
+  const { stderr, exitCode} = await $`git merge --squash origin/${sourceBranch}`.nothrow()
+
+  if (stderr.toString().includes("Automatic merge went well")) {
+    core.info(`Committing changes...`);
+    await $`git commit -m "${commitMessage}"`;
+    core.info(`Pushing changes to ${targetBranch}...`);
+    await $`git push origin ${targetBranch}`;
+    core.notice(`Commit has been squashed merged from "${sourceBranch}" to "${targetBranch}"`);
+    return successMergeResult();
+  }
+
+
+
+  if (stderr.toString().includes("CONFLICT")) {
+    core.error(`Failed to merge commit from "${sourceBranch}" to "${targetBranch}". Conflict detected`);
+    return failMergeResult(stderr.toString());
+  }
+
+  core.error(`Failed to merge commit from "${sourceBranch}" to "${targetBranch}"`);
+  return failMergeResult(stderr.toString());
+}
