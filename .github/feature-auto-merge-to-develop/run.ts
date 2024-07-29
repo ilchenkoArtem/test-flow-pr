c1004c2ef96819a72869e251cab7f7a71cbc0830
ilchenkoArtem <artem.ilchenko@711media.de>
2024-07-29 17:44:26 +0300

    fix

--- .github/feature-auto-merge-to-develop/run.ts
+++ .github/feature-auto-merge-to-develop/run.ts
@@ -12,116 +12,115 @@ const MERGED_PR_NUMBER = process.env.PR_NUMBER;
 
 const octokit = github.getOctokit(TOKEN);
 
-core.info(`Getting merged pull request info...`);
-const {data: triggerPullRequest} = await octokit.rest.pulls.get({
-  owner: github.context.repo.owner,
-  repo: github.context.repo.repo,
-  pull_number: parseFloat(MERGED_PR_NUMBER),
-});
-
-if (!triggerPullRequest) {
-  core.setFailed(`Pull request #${MERGED_PR_NUMBER} is not found`);
-  process.exit(1);
-}
-
-core.startGroup(`Info:`);
-core.info(`URL: ${triggerPullRequest.html_url}`);
-core.info(`Title: ${triggerPullRequest.title}`);
-core.info(`Merged at: ${new Date(triggerPullRequest.merged_at).toLocaleDateString()}`);
-core.endGroup();
-
-/* --------------------- Get last merged pull request info --------------------- */
-core.startGroup(`Last merged pull request info:`);
-core.info(`Fetching pull requests merged into develop from ${BASE_BRANCH}...`);
-const {data: closedPullRequestsByHeadBranch} = await octokit.rest.pulls.list({
-  owner: github.context.repo.owner,
-  repo: github.context.repo.repo,
-  state: 'closed',
-  head: HEAD_BRANCH,
-  baseTitle: "develop",
-  sort: 'updated',
-  direction: 'desc',
-});
-
-if (closedPullRequestsByHeadBranch.length === 0) {
-  core.notice(`No pull requests found for ${HEAD_BRANCH}. Skipping...`);
-  process.exit()
-}
-
-const mergedPullRequestsByHeadBranch = closedPullRequestsByHeadBranch.filter(
-  (pr) => !!pr.merged_at
-);
-
-if (mergedPullRequestsByHeadBranch.length === 0) {
-  core.notice(`No merged pull requests found for ${HEAD_BRANCH}. Skipping...`);
-  process.exit()
-}
-
-const parentPullRequest = mergedPullRequestsByHeadBranch[0];
-const parentPullRequestMergeCommit = parentPullRequest.merge_commit_sha;
-const parentPullRequestMergeBaseBranch = parentPullRequest.base.ref;
-
-core.info(`URL: ${parentPullRequest.html_url}`);
-core.info(`Title: ${parentPullRequest.title}`);
-core.info(`Merged at: ${new Date(parentPullRequest.merged_at).toLocaleDateString()}`);
-core.info(`Merge commit SHA: ${parentPullRequestMergeCommit}`);
-core.info(`Merged to branch: ${parentPullRequestMergeBaseBranch}`);
-core.endGroup()
-
-if (!parentPullRequestMergeCommit) {
-  core.setFailed(`Merge commit SHA is not found for the last merged pull request`);
-  process.exit(1);
-}
-
-core.startGroup(`Reverting commit "${parentPullRequestMergeCommit}"...`);
-await revertCommit({
-  branchForRevert: parentPullRequestMergeBaseBranch,
-  commitToRevert: parentPullRequestMergeCommit,
-  gitHubToken: TOKEN,
-  repoFullName: `${github.context.repo.owner}/${github.context.repo.repo}`,
-})
-core.endGroup()
-
-core.startGroup(`Creating new pull request based on the parent pull request "${parentPullRequestMergeBaseBranch}"...`);
-const mergeTitleInfo = mergeTitle(parentPullRequest.title, triggerPullRequest.title);
-
 try {
-  const {data: createdPullRequest} = await octokit.rest.pulls.create({
+  core.info(`Getting merged pull request info...`);
+  const {data: triggerPullRequest} = await octokit.rest.pulls.get({
     owner: github.context.repo.owner,
     repo: github.context.repo.repo,
-    head: parentPullRequest.head.ref,
-    base: parentPullRequest.base.ref,
-    title: mergeTitleInfo.merged ? mergeTitleInfo.title : parentPullRequest.title,
-    body: `This PR is created automatically after the revert of PR [${parentPullRequest.title}](${parentPullRequest.url}) from ${parentPullRequest.base.ref}.`,
+    pull_number: parseFloat(MERGED_PR_NUMBER),
   });
+
+  if (!triggerPullRequest) {
+    core.setFailed(`Pull request #${MERGED_PR_NUMBER} is not found`);
+    process.exit(1);
+  }
+
+  core.startGroup(`Info:`);
+  core.info(`URL: ${triggerPullRequest.html_url}`);
+  core.info(`Title: ${triggerPullRequest.title}`);
+  core.info(`Merged at: ${new Date(triggerPullRequest.merged_at).toLocaleDateString()}`);
+  core.endGroup();
+
+  /* --------------------- Get last merged pull request info --------------------- */
+  core.startGroup(`Last merged pull request info:`);
+  core.info(`Fetching pull requests merged into develop from ${BASE_BRANCH}...`);
+  const {data: closedPullRequestsByHeadBranch} = await octokit.rest.pulls.list({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    state: 'closed',
+    head: HEAD_BRANCH,
+    baseTitle: "develop",
+    sort: 'updated',
+    direction: 'desc',
+  });
+
+  if (closedPullRequestsByHeadBranch.length === 0) {
+    core.notice(`No pull requests found for ${HEAD_BRANCH}. Skipping...`);
+    process.exit()
+  }
+
+  const mergedPullRequestsByHeadBranch = closedPullRequestsByHeadBranch.filter(
+    (pr) => !!pr.merged_at
+  );
+
+  if (mergedPullRequestsByHeadBranch.length === 0) {
+    core.notice(`No merged pull requests found for ${HEAD_BRANCH}. Skipping...`);
+    process.exit()
+  }
+
+  const parentPullRequest = mergedPullRequestsByHeadBranch[0];
+  const parentPullRequestMergeCommit = parentPullRequest.merge_commit_sha;
+  const parentPullRequestMergeBaseBranch = parentPullRequest.base.ref;
+
+  core.info(`URL: ${parentPullRequest.html_url}`);
+  core.info(`Title: ${parentPullRequest.title}`);
+  core.info(`Merged at: ${new Date(parentPullRequest.merged_at).toLocaleDateString()}`);
+  core.info(`Merge commit SHA: ${parentPullRequestMergeCommit}`);
+  core.info(`Merged to branch: ${parentPullRequestMergeBaseBranch}`);
+  core.endGroup()
+
+  if (!parentPullRequestMergeCommit) {
+    core.setFailed(`Merge commit SHA is not found for the last merged pull request`);
+    process.exit(1);
+  }
+
+  core.startGroup(`Reverting commit "${parentPullRequestMergeCommit}"...`);
+  await revertCommit({
+    branchForRevert: parentPullRequestMergeBaseBranch,
+    commitToRevert: parentPullRequestMergeCommit,
+    gitHubToken: TOKEN,
+    repoFullName: `${github.context.repo.owner}/${github.context.repo.repo}`,
+  })
+  core.endGroup()
+
+  core.startGroup(`Creating new pull request based on the parent pull request "${parentPullRequestMergeBaseBranch}"...`);
+  const mergeTitleInfo = mergeTitle(parentPullRequest.title, triggerPullRequest.title);
+
+  try {
+    const {data: createdPullRequest} = await octokit.rest.pulls.create({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      head: parentPullRequest.head.ref,
+      base: parentPullRequest.base.ref,
+      title: mergeTitleInfo.merged ? mergeTitleInfo.title : parentPullRequest.title,
+      body: `This PR is created automatically after the revert of PR [${parentPullRequest.title}](${parentPullRequest.url}) from ${parentPullRequest.base.ref}.`,
+    });
+  } catch (e) {
+    core.setFailed(e.message);
+    process.exit(1);
+  }
+
+
+  if (mergeTitleInfo.merged === true) {
+    core.info("PR created successfully:");
+    core.info(`Title: ${mergeTitleInfo.title}`);
+    core.info(`URL: ${triggerPullRequest.html_url}`);
+  } else {
+    core.notice(`Can't merge pull requests titles automatically ${mergeTitleInfo.reason}`);
+    throw Error(`Please verify/update the title of the new PR(${triggerPullRequest.html_url}) and merge manually`);
+  }
+  core.endGroup();
+
+
+  core.startGroup(`Add revert comment to parent PR #${parentPullRequest.number}...`);
+  await octokit.rest.issues.createComment({
+    body: `This PR has been reverted by PR #${MERGED_PR_NUMBER}.`,
+    issue_number: parentPullRequest.number,
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+  });
+  core.endGroup();
 } catch (e) {
   core.setFailed(e.message);
   process.exit(1);
 }
-
-
-if (mergeTitleInfo.merged === true) {
-  core.info("PR created successfully:");
-  core.info(`Title: ${mergeTitleInfo.title}`);
-  core.info(`URL: ${triggerPullRequest.html_url}`);
-} else {
-  core.error(`Can't merge pull requests titles automatically ${mergeTitleInfo.reason}`);
-  core.setFailed(`Please verify/update the title of the new PR(${triggerPullRequest.html_url}) and merge manually`);
-}
-core.endGroup();
-
-
-core.startGroup(`Add revert comment to parent PR #${parentPullRequest.number}...`);
-await octokit.rest.issues.createComment({
-  body: `This PR has been reverted by PR #${MERGED_PR_NUMBER}.`,
-  issue_number: parentPullRequest.number,
-  owner: github.context.repo.owner,
-  repo: github.context.repo.repo,
-});
-core.endGroup();
-
-
-
-
-
-
