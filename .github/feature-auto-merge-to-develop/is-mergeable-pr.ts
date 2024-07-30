3dfed51115996200165bd9723f038b99798397b9
ilchenkoArtem <artem.ilchenko@711media.de>
2024-07-30 13:57:16 +0300

    revert

--- .github/feature-auto-merge-to-develop/is-mergeable-pr.ts
+++ .github/feature-auto-merge-to-develop/is-mergeable-pr.ts
@@ -1,6 +1,8 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
 
+const STATUS_CHECK_INTERVAL_MS = 7000;
+
 interface IsMergeablePrArgs {
   prNumber: number;
   githubToken: string;
@@ -11,21 +13,26 @@ export const isMergeable = ({prNumber, githubToken}: IsMergeablePrArgs) => {
 
   //We need to check if the PR is mergeable with promise because GitHub API returns null
   //for mergeable field when it is computing the mergeability
-  return new Promise<boolean>(async (resolve, reject) => {
-    try {
-      const {data: pr} = await octokit.rest.pulls.get({
-        owner: github.context.repo.owner,
-        repo: github.context.repo.repo,
-        pull_number: prNumber,
-      });
+  return new Promise<boolean>( (resolve, reject) => {
+    const checkMergeable = async () => {
+      try {
+        const {data: pr} = await octokit.rest.pulls.get({
+          owner: github.context.repo.owner,
+          repo: github.context.repo.repo,
+          pull_number: prNumber,
+        });
 
-      if (pr.mergeable === null) {
-        core.info(`GitHub has started a background job to compute the mergeability. Waiting...`);
-        return new Promise((resolve) => setTimeout(resolve, 7000));
+        if (pr.mergeable === null) {
+          core.info(`GitHub has started a background job to compute the mergeability. Recheck after ${STATUS_CHECK_INTERVAL_MS / 1000 } seconds...`);
+          setTimeout(checkMergeable, STATUS_CHECK_INTERVAL_MS);
+          return;
+        }
+        resolve(pr.mergeable);
+      } catch (error) {
+        reject(error);
       }
-      resolve(pr.mergeable);
-    } catch (error) {
-      reject(error);
     }
+
+    checkMergeable();
   })
 }
