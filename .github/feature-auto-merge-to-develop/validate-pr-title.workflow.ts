a3c43a7586158b6f29d54aba6cc364b1379efe1f
ilchenkoArtem <artem.ilchenko@711media.de>
2024-07-30 12:49:03 +0300

    add title validator

---
+++ .github/feature-auto-merge-to-develop/validate-pr-title.workflow.ts
@@ -0,0 +1,13 @@
+import * as core from '@actions/core';
+import {exitWithError, getEnv} from './utils';
+import {isValidPullRequestTitle} from './pull-request-title-utils';
+
+const PR_TITLE = getEnv(process.env.PR_TITLE);
+const isValid = isValidPullRequestTitle(PR_TITLE);
+
+if (isValid) {
+  core.notice(`Pull request title "${PR_TITLE}" is valid.`);
+} else {
+  exitWithError(`Pull request title "${PR_TITLE}" is invalid."`);
+}
+
