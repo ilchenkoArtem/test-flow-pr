c8c9f20da36ac6bfe908146e7afa76d1bb770d0d
ilchenkoArtem <artem.ilchenko@711media.de>
2024-07-30 11:25:23 +0300

    fix

--- .github/feature-auto-merge-to-develop/utils.ts
+++ .github/feature-auto-merge-to-develop/utils.ts
@@ -2,7 +2,7 @@ import * as core from '@actions/core';
 
 export const getEnv = (key: string): string => {
   const value = process.env[key];
-  if (!value) {
+  if (value === undefined) {
     core.setFailed(`Env variable ${key} is required`);
     process.exit(1);
   }
