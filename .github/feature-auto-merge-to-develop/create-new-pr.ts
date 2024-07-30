2ef6928f126b944bde0a1e7230175dfb05eadadd
ilchenkoArtem <artem.ilchenko@711media.de>
2024-07-30 13:18:19 +0300

    fix

--- .github/feature-auto-merge-to-develop/create-new-pr.ts
+++ .github/feature-auto-merge-to-develop/create-new-pr.ts
@@ -16,30 +16,55 @@ interface CreateNewPrArgs {
 export const createNewPullRequestByParent = async ({githubToken, parentPullRequest, title}: CreateNewPrArgs) => {
   const octokit = github.getOctokit(githubToken);
 
-  const {data: createdPullRequest} = await octokit.rest.pulls.create({
+  try {
+    const {data: createdPullRequest} = await octokit.rest.pulls.create({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      head: parentPullRequest.headRef,
+      base: parentPullRequest.baseRef,
+      title: title,
+      body: `This PR is created automatically after the revert of PR [${parentPullRequest.title}](${parentPullRequest.htmlUrl}) from ${parentPullRequest.baseRef}.`,
+    });
+
+    core.info(`Pull request created: ${createdPullRequest.html_url}`);
+    core.info(`Title: ${createdPullRequest.title}`);
+
+    core.startGroup(`Add revert comment to parent PR(${parentPullRequest.htmlUrl})`);
+    core.info("Adding comment...");
+    await octokit.rest.issues.createComment({
+      body: `Created an updated pull request [${createdPullRequest.title}](${parentPullRequest.headRef}})`,
+      issue_number: parentPullRequest.number,
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+    });
+    core.info("Successfully added")
+    core.endGroup();
+
+    return createdPullRequest;
+  } catch (error) {
+    core.info(error);
+    throw error;
+  }
+}
+
+
+
+const getIfExistOrCreateNewPR = async ({githubToken, title, parentPullRequest}: CreateNewPrArgs) => {
+  const TOKEN = github.getOctokit(githubToken);
+  const body = {
     owner: github.context.repo.owner,
     repo: github.context.repo.repo,
     head: parentPullRequest.headRef,
     base: parentPullRequest.baseRef,
     title: title,
     body: `This PR is created automatically after the revert of PR [${parentPullRequest.title}](${parentPullRequest.htmlUrl}) from ${parentPullRequest.baseRef}.`,
-  });
+  };
 
-  core.info(`Pull request created: ${createdPullRequest.html_url}`);
-  core.info(`Title: ${createdPullRequest.title}`);
-
-
-
-  core.startGroup(`Add revert comment to parent PR(${parentPullRequest.htmlUrl})`);
-  core.info("Adding comment...");
-  await octokit.rest.issues.createComment({
-    body: `Created an updated pull request [${createdPullRequest.title}](${parentPullRequest.headRef}})`,
-    issue_number: parentPullRequest.number,
+  const {data: pullRequests} = await TOKEN.rest.pulls.list({
     owner: github.context.repo.owner,
     repo: github.context.repo.repo,
+    head: parentPullRequest.headRef,
+    base: parentPullRequest.baseRef,
+    state: 'open',
   });
-  core.info("Successfully added")
-  core.endGroup();
-
-  return createdPullRequest;
-}
+};
