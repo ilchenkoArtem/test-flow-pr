37b1b0b4ac3d34f5e156018077f8be19462f8c7b
ilchenkoArtem <artem.ilchenko@711media.de>
2024-07-30 15:33:58 +0300

    fix

--- .github/feature-auto-merge-to-develop/revert-and-merge-feature.workflow.ts
+++ .github/feature-auto-merge-to-develop/revert-and-merge-feature.workflow.ts
@@ -45,7 +45,7 @@ const {data: closedPullRequestsByHeadBranch} = await octokit.rest.pulls.list({
   state: 'closed',
   head: `${REQUEST_DATA.owner}:${BASE_BRANCH}`,
   base: "develop",
-  sort: 'updated',
+  sort: 'created',
   per_page: 10,
 });
 
