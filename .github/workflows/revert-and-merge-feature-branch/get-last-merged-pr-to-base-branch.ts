7dd19de53cd688de730b5acd57ae439be2dd5a2f
ilchenkoArtem <artem.ilchenko@711media.de>
2024-08-01 19:31:38 +0300

    fix

--- .github/workflows/revert-and-merge-feature-branch/get-last-merged-pr-to-base-branch.ts
+++ .github/workflows/revert-and-merge-feature-branch/get-last-merged-pr-to-base-branch.ts
@@ -1,6 +1,8 @@
 import * as core from '@actions/core';
-import {OUTPUT_NAME} from './utils';
+import {getEnv, OUTPUT_NAME} from './utils';
 
 const PR_WHICH_TRIGGERED_ACTION = core.getInput(OUTPUT_NAME.EVENT_PULL_REQUEST_INFO);
+const PR_WHICH_TRIGGERED_ACTION2 = getEnv(OUTPUT_NAME.EVENT_PULL_REQUEST_INFO);
 
 console.log('PR_WHICH_TRIGGERED_ACTION', PR_WHICH_TRIGGERED_ACTION);
+console.log('PR_WHICH_TRIGGERED_ACTION2', PR_WHICH_TRIGGERED_ACTION2);
