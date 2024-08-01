e7f79b15cef6abd5083df2fae78fd123fbbfa3c8
ilchenkoArtem <artem.ilchenko@711media.de>
2024-08-01 13:17:50 +0300

    fix

--- .github/actions/teams-notification/index.ts
+++ .github/actions/teams-notification/index.ts
@@ -1,6 +1,6 @@
 import {getEnv} from '../../feature-auto-merge-to-develop/utils';
 
-const WEBHOOK_URL = getEnv('webhook-url');
+const WEBHOOK_URL = getEnv('WEBHOOK_URL');
 console.log('WEBHOOK_URL', WEBHOOK_URL);
 
 
