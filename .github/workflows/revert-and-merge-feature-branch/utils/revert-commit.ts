8aba437899dfbca5cd5e4822f1722bfe14a93d4a
ilchenkoArtem <artem.ilchenko@711media.de>
2024-08-02 19:10:39 +0300

    fix

--- .github/workflows/revert-and-merge-feature-branch/utils/revert-commit.ts
+++ .github/workflows/revert-and-merge-feature-branch/utils/revert-commit.ts
@@ -14,7 +14,8 @@ interface RevertCommitArgs {
 export const revertCommit = async ({branchForRevert, commitToRevert, gitHubToken}: RevertCommitArgs):Promise<boolean> => {
   await addGitConfig({gitHubToken});
 
-  const currentBranch = await $`git branch`;
+  await $`git fetch --all`.throws(true)
+  const currentBranch = await $`git branch --show-current`;
   console.log("stdout", currentBranch.stdout.toString())
   console.log("stderr", currentBranch.stderr.toString())
   core.info(`Current branch: ${currentBranch}`)
