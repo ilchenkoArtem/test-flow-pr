b054188b258f0f51a49a26fd8a1251d6649e2b72
ilchenkoArtem <artem.ilchenko@711media.de>
2024-08-02 13:11:15 +0300

    fix

--- .github/workflows/revert-and-merge-feature-branch/revert-last-merged-pr.step.ts
+++ .github/workflows/revert-and-merge-feature-branch/revert-last-merged-pr.step.ts
@@ -5,9 +5,6 @@ import * as core from '@actions/core';
 
 const lastMergedPr = getEnvJson<PullRequest>("LAST_MERGED_PR");
 
-console.log('typeof lastMergedPr', typeof lastMergedPr);
-core.debug(`Input Last Merged PR: ${JSON.stringify(lastMergedPr)}`);
-
 core.info(`Revert commit "${lastMergedPr.merge_commit_sha}" from "${lastMergedPr.base.ref}"`);
 const reverted = await revertCommit({
   branchForRevert: lastMergedPr.base.ref,
