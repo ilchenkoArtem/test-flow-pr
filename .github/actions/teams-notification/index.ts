8604ffab50c6742060cc0eb7c679eee7d99da200
ilchenkoArtem <artem.ilchenko@711media.de>
2024-08-03 01:35:22 +0300

    fix

--- .github/actions/teams-notification/index.ts
+++ .github/actions/teams-notification/index.ts
@@ -1,3 +1,4 @@
+import * as core from '@actions/core';
 import {getEnv, getEnvJson, isValidJson} from '../../workflows/utils/helpers';
 import {createMessage} from '.github/teams-notification/Message';
 import {getTeamsMentionByGitUser, sendNotification} from '.github/teams-notification/utils';
@@ -10,6 +11,15 @@ const body = isValidJson(process.env.BODY) ? getEnvJson<Record<string, unknown>[
 const type = getEnv('TYPE', false)
 const actions = getEnvJson<Record<string, unknown>[]>('ACTIONS', false)
 const mentionList = getEnv('MENTION_LIST', false)?.split('|').map((mention) => mention.trim())
+
+core.debug(`Sending notification to Teams with webhook: ${webhook}`)
+core.debug(`Title: ${title}`)
+core.debug(`Body: ${body}`)
+core.debug(`Type: ${type}`)
+core.debug(`Actions: ${actions}`)
+core.debug(`Mention list: ${mentionList}`)
+core.debug(`Mention list: ${mentionList}`)
+
 
 const message = createMessage({
   mentionList: getTeamsMentionByGitUser(mentionList || []),
