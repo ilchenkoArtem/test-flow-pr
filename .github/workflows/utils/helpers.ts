7557c71f02c9e0b4934a57d5959247df2fa4df45
ilchenkoArtem <artem.ilchenko@711media.de>
2024-08-03 01:58:33 +0300

    fix

--- .github/workflows/utils/helpers.ts
+++ .github/workflows/utils/helpers.ts
@@ -18,10 +18,11 @@ export function getEnv(key: string, required: true): string;
 export function getEnv(key: string): string;
 export function getEnv(key: string, required = true): string | null {
   const value = process.env[key];
-  if (!value && required) {
+  if (value === undefined && required) {
     exitWithError(`Environment variable "${key}" is not set`);
   }
-  if (value === undefined) return null;
+
+  if (value === undefined || (!required && value === "")) return null;
   return value.trim();
 }
 
