4ece2320f6870944f37b818cd5fd1d023f2f579e
ilchenkoArtem <artem.ilchenko@711media.de>
2024-08-03 01:18:53 +0300

    fix

--- .github/actions/teams-notification/index.ts
+++ .github/actions/teams-notification/index.ts
@@ -5,7 +5,7 @@ const WEBHOOK_URL = getEnv('WEBHOOK_URL');
 const BODY = isValidJson(process.env.BODY) ? getEnvJson<Record<string, unknown>[]>("BODY") : getEnv('BODY')
 const TYPE = getEnv('TYPE', false)
 const ACTIONS = getEnvJson<Record<string, unknown>[]>('ACTIONS', false)
-const NOTIFY = getEnvJson<string[]>('NOTIFY', false)
+const MENTION_LIST = getEnv('MENTION_LIST', false)?.split('|').map((mention) => mention.trim())
 
 
 
@@ -15,7 +15,7 @@ console.log('WEBHOOK_URL', WEBHOOK_URL);
 console.log('BODY', BODY);
 console.log('TYPE', TYPE);
 console.log('ACTIONS', ACTIONS);
-console.log('NOTIFY', NOTIFY);
+console.log('MENTION_LIST', MENTION_LIST);
 
 
 
