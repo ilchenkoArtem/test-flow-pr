3621ef06614ff063b58df0705e4e9b7fff033ae4
ilchenkoArtem <artem.ilchenko@711media.de>
2024-08-02 23:01:50 +0300

    test

--- .github/actions/teams-notification/index.ts
+++ .github/actions/teams-notification/index.ts
@@ -1,11 +1,11 @@
 
-import {context} from '@actions/github';
+import * as github from '@actions/github';
 import {getEnv} from '../../feature-auto-merge-to-develop/utils';
 
 const WEBHOOK_URL = getEnv('WEBHOOK_URL');
 console.log('WEBHOOK_URL', WEBHOOK_URL);
 
-console.log('context', context);
-console.log('context.actor', context.actor);
-
+console.log('context', github.context);
+console.log('context.actor', github.context.actor);
+console.log('context.actor', github.context.actor);
 
