5541c9c5da8a7bf32c2a1d42c707d44b78ff985b
ilchenkoArtem <artem.ilchenko@711media.de>
2024-08-06 01:20:16 +0300

    update index.ts file

--- index.ts
+++ index.ts
@@ -1,21 +1,2 @@
 const test = 1;
-
-const test = 2;
-
-const test3 = 6;
-const test4 = 8;
-const test5 = 10;
-const test6 = 12;
-const test7 = 13;
-const test8 = 14;
-const test9 = 20;
-const test10 = 21;
-const test11 = 22;
-const test12 = 20;
-const test13 = 22;
-const test22 = 22;
-const test23 = 23;
-const test24 = 24;
-const test25 = 26;
-const test27 = 27;
-const test28 = 28;
+const test2 = 2;
